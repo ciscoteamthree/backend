4adf85bcab657813665eaed6a7d20f75b574414f
Orhan Hirsch <orhanhenrik@gmail.com>
2019-10-25 17:42:10 +0200

    fix editMeeting

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
 const Server = require('socket.io');
 const io = new Server();
 const { templates } = require('./templates');
-const { meeting } = require('./meeting');
+const { meeting: initialMeeting } = require('./meeting');
+
 var rp = require('request-promise');
 io.set('origins', '*:*');
+
 const dotenv = require('dotenv');
 dotenv.config();
 
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
-//let meeting = null;
 let auth = null;
 /*
 { access_token, expires_in, refresh_token, refresh_token_expires_in }
 */
+
 if (process.env.TOKEN) {
   auth = {
     access_token: process.env.TOKEN
   };
 }
 
+let meeting = initialMeeting;
+
 const redirectUri = encodeURIComponent(process.env.REDIRECT_URL);
 const loginUrl = `https://api.ciscospark.com/v1/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=spark%3Akms%20spark%3Aall%20spark-admin%3Adevices_read%20spark-compliance%3Arooms_read&state=set_state_here`;
 
+const netatmoAccessToken = process.env.NETATMO_ACCESS_TOKEN;
+const netatmoDeviceId = encodeURIComponent(process.env.NETATMO_DEVICE_ID);
+
+const getSensorData = () => {
+  return rp(
+    `https://api.netatmo.com/api/getstationsdata?access_token=${netatmoAccessToken}&device_id=${netatmoDeviceId}`
+  )
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
 io.on('connection', socket => {
+  console.log('connect');
   socket.emit('templates', templates);
   socket.emit('currentMeeting', meeting);
   if (auth) {
@@ -34,6 +54,7 @@ io.on('connection', socket => {
   }
 
   socket.on('editMeeting', editedMeeting => {
+    console.log('editMeeting');
     meeting = editedMeeting;
     io.emit('currentMeeting', meeting);
   });
@@ -44,6 +65,7 @@ io.on('connection', socket => {
   });
 
   socket.on('oauth', code => {
+    console.log('oauth');
     rp('https://api.ciscospark.com/v1/access_token', {
       method: 'POST',
       json: {
